0e643c890d84243e0cbb1766f2e71de444c66b0c
kwonsoonook <sunoogy99@naver.com>
2024-05-14 15:30:16 +0900

    Feat: 프로필 레이아웃 작업
    appBar, 프로필 헤더 부분 구현
    차트 시각화는 fl_chart 라이브러리 이용
    아직 목업 데이터 생성하지 않음
    
    Related to #5

--- lib/screens/my_profile_screen.dart
+++ lib/screens/my_profile_screen.dart
@@ -1,4 +1,7 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 // 내 프로필 화면
 class MyProfileScreen extends StatefulWidget {
@@ -11,8 +14,114 @@ class MyProfileScreen extends StatefulWidget {
 class _MyProfileScreenState extends State<MyProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('내 프로필 화면'),),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('프로필'),
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              onPressed: () {
+                // 설정 화면으로 이동
+              },
+              icon: const Icon(Icons.settings_outlined),
+            ),
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 내 프로필 및 프로필 수정 버튼
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 프로필 사진
+                  const Icon(
+                    Icons.account_circle,
+                    size: 50,
+                  ),
+
+                  const SizedBox(width: 15),
+                  // 닉네임, 랭크
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'sunoogy',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          'silver',
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black.withOpacity(0.5)),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // 프로필 수정 버튼
+                  GestureDetector(
+                    onTap: () {
+                      // 프로필 수정 화면으로 이동
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 7, vertical: 3),
+                      decoration: ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          side: BorderSide(
+                              width: 1, color: Colors.black.withOpacity(0.5)),
+                        ),
+                      ),
+                      child: const Text(
+                        'Edit Profile >',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Nunito',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // 주간 점수 차트
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Column(
+                children: [
+                  const Text(
+                    'Weekly Score Chart',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  // 차트
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
+
+  // Widget _buildLineChart() {
+  //
+  // }
 }
